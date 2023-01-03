9fc5a4f7ddfa624ab2c6b578c90dfc5b8f1cd9d8
hayhyang <hayhyang@gmail.com>
2023-01-03 19:09:55 +0900

    [class] 01.클래스 정의

---
+++ ECMAScript6/7.class/01_class_definition.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+
+    sayHi() {
+        console.log(`Hi! ${this._name}`)
+    }
+}
+
+const me = new Person('Lee')
+me.sayHi()
+
+console.log(me instanceof Person)
+
+const Foo = class Myclass {}
+
+const foo = new Foo()
+console.log("foo", foo)
+
+// new Myclass()
